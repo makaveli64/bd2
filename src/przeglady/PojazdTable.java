ee7c3f4032c206db1410c4cd463b9ff86730c982
Konrad <makaveli64@gmail.com>
2015-06-17 13:26:01 +0200

    Plan serwisowy part 1

--- src/przeglady/PojazdTable.java
+++ src/przeglady/PojazdTable.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
+import java.util.Vector;
 
 /**
  * Created by user
@@ -15,8 +16,11 @@ public class PojazdTable extends JPanel {
 
     JButton updateButton;
     JButton deleteButton;
+    JButton planSerwisowyButton;
 
     String[] rejestracje;
+    Vector<String> columnNames;
+    Vector<Vector> data;
 
     PojazdTable(JTable table) {
         super();
@@ -30,15 +34,22 @@ public class PojazdTable extends JPanel {
 
         updateButton = new JButton();
         deleteButton = new JButton();
+        planSerwisowyButton = new JButton();
 
         updateButton.setText("Modyfikuj");
         deleteButton.setText("Usuń");
+        planSerwisowyButton.setText("Wyświetl plan serwisowy");
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(updateButton);
         buttonPanel.add(deleteButton);
 
+        JPanel buttonPanel2 = new JPanel();
+        buttonPanel2.add(planSerwisowyButton);
+
         add(buttonPanel);
+        add(buttonPanel2);
+
         rejestracje = new String[this.table.getRowCount()];
 
         for (int i = 0; i < rejestracje.length; i++)
@@ -60,6 +71,17 @@ public class PojazdTable extends JPanel {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     delete();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+        planSerwisowyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    planSerwisowy();
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
@@ -121,6 +143,66 @@ public class PojazdTable extends JPanel {
             } catch (Exception e) {
                 throw new SQLException(e);
             }
+        }
+    }
+
+    private void planSerwisowy() throws Exception {
+        JDBCConnector connector = new JDBCConnector();
+        Connection connection;
+        PreparedStatement ps;
+        ResultSet rs;
+        ResultSetMetaData md;
+
+        int[] selectedRows = table.getSelectedRows();
+
+        if (selectedRows.length > 1)
+            throw new Exception("Należy wybrać jeden pojazd");
+
+        String query = "SELECT \"przebieg\", \"nazwa\" FROM \"Pozycja_planu_serwisowego\" PPS " +
+                "JOIN \"Pojazd\" P ON PPS.\"id_modelu\" = P.\"id_modelu\" " +
+                "JOIN \"Model\" M ON P.\"id_modelu\" = M.\"id_modelu\" " +
+                "WHERE \"rejestracja\" LIKE ?";
+
+        try {
+            DriverManager.registerDriver(new OracleDriver());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage() + "Cannot find the proper driver");
+            System.exit(-1);
+        }
+//        System.out.println(rejestracje[selectedRows[0]]);
+        try {
+            connection = connector.getConnection();
+            ps = connection.prepareStatement(query);
+            ps.setString(1, rejestracje[selectedRows[0]]);
+            rs = ps.executeQuery();
+
+            columnNames = new Vector<String>();
+            data = new Vector<Vector>();
+
+            if (rs != null) {
+                md = rs.getMetaData();
+                Vector<Object> column;
+
+                while (rs.next()) {
+                    column = new Vector<Object>();
+                    for (int i = 1; i < md.getColumnCount() + 1; i++)
+                        column.add(rs.getString(i));
+                    data.add(column);
+                }
+
+                for (int i = 1; i < md.getColumnCount() + 1; i++)
+                    columnNames.add(md.getColumnName(i));
+
+                if (!columnNames.isEmpty())
+                    table = new JTable(data, columnNames);
+                else
+                    columnNames.add("No rows extracted");
+
+                table = new JTable(data, columnNames);
+                PojazdTable.start(table);
+            }
+        } catch (Exception e) {
+            throw new SQLException(e);
         }
     }
 
